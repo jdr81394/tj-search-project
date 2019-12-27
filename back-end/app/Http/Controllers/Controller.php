dc63bd82397e2c8b7d156c6fcc74241fefcc1a48
JDR <42478354+jdr81394@users.noreply.github.com>
2019-12-27 14:13:58 -0500

    got mail to work

--- back-end/app/Http/Controllers/Controller.php
+++ back-end/app/Http/Controllers/Controller.php
@@ -12,6 +12,16 @@ use Illuminate\Support\Facades\Log;
 
 use DB;
 use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\MailException;
+
+// Load Composer's autoloader
+// require '/../../vendor/autoload.php';
+
+// require '../../vendor/autoload.php';
+
+
 
 class Controller extends BaseController
 {
@@ -136,11 +146,100 @@ class Controller extends BaseController
     public function store(Request $request) {
         try {
 
-            Log::info("reached send email");
+            $mail = new PHPMailer(true);
 
-            mail('jdr81394@gmail.com', 'test', 'testtt', 'headers test');
+            $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host = 'smtp.gmail.com';                             // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'jdr81394@gmail.com';                   // SMTP username
+            $mail->Password   = 'Bbknights1!';                          // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+        
+            //Recipients
+            $mail->setFrom('jdr81394@gmail.com', 'Mailer');
+            $mail->addAddress('jdr81394@gmail.com', 'Joe User');     // Add a recipient
+            $mail->addAddress('jdr81394@gmail.com');               // Name is optional
+            $mail->addReplyTo('info@example.com', 'Information');
+            $mail->addCC('jdr81394@gmail.com');
+            $mail->addBCC('bcc@example.com');
+          $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            // Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $mail->send();
+            echo 'Message has been sent';
 
-        } catch(Exception $e) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // // Instantiation and passing `true` enables exceptions
+            // $mail = new PHPMailer(true);
+            // // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            // $mail->SMTPDebug = 2;
+
+            // $mail->isSMTP();                                            // Send using SMTP
+            // $mail->Host       = '0.0.0.0:9000';                 // Set the SMTP server to send through
+            // $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            // $mail->Username   = 'jake@surge.com';                       // SMTP username
+            // $mail->Password   = 'secret';                               // SMTP password
+            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            // $mail->Port       = 587;                                    // TCP port to connect to
+            // $mail->SMTPOptions = array(
+            //     'ssl' => array(
+            //         'verify_peer' => false,
+            //         'verify_peer_name' => false,
+            //         'allow_self_signed' => true
+            //     )
+            // );
+
+            // //Recipients
+            // $mail->setFrom('from@example.com', 'Mailer');
+            // $mail->addAddress('jdr81394@gmail.com', 'Joe User');     // Add a recipient
+            // $mail->addAddress('jdr81394@gmail.com');               // Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            // // Attachments
+            // // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // // Content
+            // $mail->isHTML(true);                                  // Set email format to HTML
+            // $mail->Subject = 'Here is the subject';
+            // $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            // $mail->send();
+            // echo 'Message has been sent';
+
+        } catch(MailException $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 
         }
     }
