5ef49b63f330fcbe1f8b39dd32267e1f770cb34b
JDR <42478354+jdr81394@users.noreply.github.com>
2019-12-27 10:37:58 -0500

    moved search items into app, deleted search items, going to go back to fix routing

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemsServiceService } from './items-service/items-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+
+  cart: String[];
+  basicSearch: String;
+  result: String[]
+
+  constructor(
+    private itemsService: ItemsServiceService
+  ) { 
+    this.cart = [];
+    this.basicSearch = '';
+    this.result = [];
+  }
+
+  addToCart(item) {
+    console.log('item is in cart:  ' ,item);
+    this.cart.push(item);
+    console.log("cart:: "  ,this.cart);
+  }
+
+  getItems() {
+    console.log("basic search:  "  ,this.basicSearch);
+    this.itemsService.getItems(this.basicSearch).subscribe(
+      (res: String[]) => {
+        this.result = res;
+        console.log("this result:   "  ,this.result);
+        return this.result;
+
+      }
+    )
+  }
+
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import  { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { SearchItemsModule } from './search-items/search-items.module';
 import { CartModule } from './cart/cart.module';
 
 import { HttpModule } from '@angular/http';
-// import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,11 +17,11 @@ import { HttpModule } from '@angular/http';
   ],
   imports: [
     HttpModule,
-    // HttpClientModule,
+    FormsModule,
+    HttpClientModule,
     // HttpClient,
     BrowserModule,
     CartModule,
-    SearchItemsModule,
     AppRoutingModule
   ],
   providers: [],
