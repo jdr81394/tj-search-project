b3e7eb01adacea26877abe2630e2ca6b9f73d10f
JDR <42478354+jdr81394@users.noreply.github.com>
2019-12-26 19:42:03 -0500

    working on routing ;

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'cart',
+    loadChildren: './cart/cart.module#CartModule'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
