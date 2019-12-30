f26570286e57f5f89102a9d8558f86a2464916c7
JDR <42478354+jdr81394@users.noreply.github.com>
2019-12-30 13:58:49 -0500

    changed migrations, going to merge into master and work on pagination

--- back-end/database/migrations/2019_12_16_171056_create_items_table.php
+++ back-end/database/migrations/2019_12_16_171056_create_items_table.php
@@ -16,7 +16,6 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('quantity');
             $table->timestamps();
         });
     }
--- back-end/database/migrations/2019_12_16_174425_create_users_table.php
+++ back-end/database/migrations/2019_12_16_174425_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('password');
-            $table->json('cart');
             $table->timestamps();
         });
     }
